2643eab07b161e67633575cf7c927c91122e3261
jorius <josedrc97@gmail.com>
2020-03-26 13:44:49 -0500

    Prettify sql code

--- bd/init-db.sh
+++ bd/init-db.sh
@@ -3,12 +3,11 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE best-web-firms;
+
     CREATE TABLE dashboard (
         id serial PRIMARY KEY,
         content VARCHAR NOT NULL
     );
- 
-    INSERT INTO dashboard (content)
-    VALUES
-    ('Hello World!!');
+
+    INSERT INTO dashboard (content) VALUES ('Hello World!!');
 EOSQL
