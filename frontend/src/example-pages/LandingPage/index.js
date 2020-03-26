961ce3ec921b860fc33b592e6265c2fee2be3e27
Juan Cuartas <equisoide@gmail.com>
2020-03-26 11:59:58 -0500

    Customized labels in landing page

--- frontend/src/example-pages/LandingPage/index.js
+++ frontend/src/example-pages/LandingPage/index.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 
 import hero9 from '../../assets/images/hero-bg/hero-9.jpg';
-import { Button, UncontrolledTooltip, Badge } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 const LandingPage = () => {
   return (
@@ -23,26 +23,16 @@ const LandingPage = () => {
               <Row>
                 <Col lg="10" className="px-0 mx-auto d-flex align-items-center">
                   <div className="text-center">
-                    <Badge
-                      pill
-                      color="info"
-                      className="px-4 text-uppercase h-auto py-1"
-                      id="ProjVersion123">
-                      FREE react Admin Dashboard Template
-                    </Badge>
-                    <UncontrolledTooltip
-                      placement="top"
-                      target="ProjVersion123">
-                      Version: 1.1.0
-                    </UncontrolledTooltip>
                     <div className="px-4 px-sm-0 text-white mt-4">
                       <h1 className="display-2 mb-5 font-weight-bold">
-                        Bamburgh React Admin Dashboard with Reactstrap Free
+                        BEST.WEB.FIRMS
                       </h1>
                       <p className="font-size-xl text-white-50 mb-3">
-                        This admin template is the perfect lite solution for
-                        building React powered applications or presentation
-                        websites.
+                        More than 1,700 web design companies, web development
+                        companies, seo marketing agencies and web service
+                        providers personally researched, tested, reviewed and
+                        ranked by experts with decades of experience in each
+                        individual industry.
                       </p>
                       <p className="text-white font-size-lg">
                         Check out the live preview to see all the available
@@ -60,38 +50,6 @@ const LandingPage = () => {
                           <span className="btn-wrapper--label">Live Demo</span>
                           <span className="btn-wrapper--icon">
                             <FontAwesomeIcon icon={['fas', 'arrow-right']} />
-                          </span>
-                        </Button>
-                        <Button
-                          tag="a"
-                          size="lg"
-                          color="warning"
-                          className="d-block d-sm-inline-block ml-0 mt-3 mt-sm-0 ml-sm-3"
-                          href="https://uifort.com/template/bamburgh-react-admin-dashboard-reactstrap-free"
-                          target="_blank"
-                          title="Download Bamburgh React Admin Dashboard with Reactstrap Free">
-                          <span className="btn-wrapper--icon">
-                            <FontAwesomeIcon icon={['fas', 'download']} />
-                          </span>
-                          <span className="btn-wrapper--label">
-                            Download now
-                          </span>
-                        </Button>
-                        <Button
-                          tag="a"
-                          href="https://uifort.com/template/bamburgh-react-admin-dashboard-reactstrap-pro"
-                          target="_blank"
-                          size="lg"
-                          color="first"
-                          className="d-block d-sm-inline-block ml-0 ml-sm-3 mt-3 mt-sm-0"
-                          title="Learn more about Bamburgh React Admin Dashboard with Reactstrap Free version">
-                          <span className="btn-wrapper--icon">
-                            <FontAwesomeIcon
-                              icon={['fas', 'external-link-alt']}
-                            />
-                          </span>
-                          <span className="btn-wrapper--label">
-                            PRO Version
                           </span>
                         </Button>
                       </div>
