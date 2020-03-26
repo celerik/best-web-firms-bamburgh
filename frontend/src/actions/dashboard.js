0af794caa8843af3ed254a5223023febbc854bc1
jorius <josedrc97@gmail.com>
2020-03-26 17:45:59 -0500

    Fix promise fetch response

--- frontend/src/actions/dashboard.js
+++ frontend/src/actions/dashboard.js
@@ -4,9 +4,7 @@ import urls from '../config/urls';
 export const getDashboardContent = () =>
   new Promise((resolve, reject) => {
     fetch(urls.dashboard)
-      .then(response => {
-        const { content } = response.json();
-        resolve(content);
-      })
+      .then(response => response.json())
+      .then(({ content }) => resolve(content))
       .catch(error => reject(error));
   });
