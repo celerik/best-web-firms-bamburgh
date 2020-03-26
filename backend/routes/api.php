74283aef01540d67e3d4a2c19be759da40a5be53
jorius <josedrc97@gmail.com>
2020-03-26 09:30:28 -0500

    Add DashboardController to API routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\DashboardController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/dashboard', 'DashboardController@index');
